806c3d8395e8e6b41516ae0a59c7b467a29ecf0c
Jon Sturm <jonimoose@gmail.com>
2013-04-04 02:15:29 -0500

    tidump: Clean up formatting and move to using the newly exported cmd73.h
    Also adds TI-84 Plus CSE support and makes things a bit more robust by actually
    checking return codes of functions for failure. Doesn't yet attempt to
    retry failed pages, it seems the silverlink gets confused when you do so.
    
    Will need further cleanup before merging with master.
    
    TODO: Either remove or allow toggling of ram dumps and perhaps add a parameter
    to only dump single pages for debugging or testing.

--- src/tidump.c
+++ src/tidump.c
@@ -23,9 +23,9 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "titools.h"
-#include "dbus_pkt.h"
-#include "cmd73.h"
+#include <cmd73.h>
 
 #define PKTSIZE 0x400
 
@@ -35,70 +35,80 @@ int main(int argc, char **argv)
 
   tt_init(argc, argv, NULL, 0, 0, 1);
 
-  uint8_t data[] = {0x7F, 00, 00, 0x40, 00, 0x40, 0x0C, 00};
+  uint16_t page = 0;
   uint8_t *buffer;
-  uint16_t bufp;
   uint16_t length;
   FILE *f;
   char *filename;
-  
+
   if (calc_handle->model == CALC_TI84P || calc_handle->model == CALC_TI83P)
   {
-      CalcInfos info;
-      ticalcs_calc_get_version(calc_handle, &info);
-      switch(info.hw_version)
-      {
-	case 1:
-	case 3:
-	  numpages = 0x74;
-	  rampage = 0x80;
-	  break;
-	case 0:
-	  numpages = 0x1F;
-	  rampage = 0x40;
-	  break;
-	case 2:
-	  numpages = 0x3F;
-	  rampage = 0x80;
-	  break;
-	default:
-	  goto exit; // if this happens the world might be ending
-      }
-      buffer = (uint8_t *)malloc(0x5000);
+    CalcInfos info;
+    ticalcs_calc_get_version(calc_handle, &info);
+    switch(info.hw_version) {
+    case 1:
+    case 3:
+      numpages = 0x74;
+      rampage = 0x80;
+      break;
+    case 0:
+      numpages = 0x1F;
+      rampage = 0x40;
+      break;
+    case 2:
+      numpages = 0x3F;
+      rampage = 0x80;
+      break;
+    case 4:
+    case 5:
+      numpages = 0xFF;
+      rampage = 0x80;
+      break;
+    default:
+      goto exit2; // if this happens the world might be ending
+    }
 
-      filename = g_strconcat(tifiles_model_to_string(calc_handle->model), ".", "rom", NULL);
-      f = fopen(filename, "wb");
-      if (f == NULL)
-	goto exit;
-      for(data[0]=0; data[0]<=numpages; data[0]++){
-	printf("Receiving page %X out of %X\n", data[0], numpages);
-	printf(" PC->TI: MEM REQ\n");
-	dbus_send(calc_handle, PC_TI83p, 0x6F, 8, (uint8_t *)&data);
-	ti73_recv_ACK_h(calc_handle, NULL);
-	ti73_recv_XDP_h(calc_handle, &length, buffer);
-	ti73_send_ACK_h(calc_handle);
-	if (fwrite(buffer, length, 1, f) < 1)
-	  goto exit;
+    buffer = (uint8_t *)malloc(0x5000);
+
+    filename = g_strconcat(tifiles_model_to_string(calc_handle->model), ".", "rom", NULL);
+    f = fopen(filename, "wb");
+
+    if (f == NULL)
+      goto exit;
+    for(page = 0; page <= numpages; page++ ) {
+
+      printf("Receiving page %X out of %X\n", page, numpages);
+
+      int failed = ti73_send_DUMP(calc_handle, page) + ti73_recv_ACK(calc_handle, NULL) +
+              ti73_recv_XDP(calc_handle, &length, buffer) + ti73_send_ACK(calc_handle);
+      if (failed) {
+        printf("Rom Dump Failed\n");
+        goto exit;
+      }
+
+      if (fwrite(buffer, length, 1, f) < 1)
+        goto exit;
       }
       //fclose(f);
       //filename = g_strconcat(tifiles_model_to_string(calc_handle->model), ".", "ram", NULL);
       //f = fopen(filename, "wb");
       //if (f == NULL)
-	//goto exit;
+        //goto exit;
       //for(data[0]=rampage; data[0]<=rampage+1; data[0]++){
-	//printf("Receiving page %X out of %X\n", data[0], rampage+1);
-	//printf(" PC->TI: MEM REQ\n");
-	//dbus_send(calc_handle, PC_TI83p, 0x6F, 8, (uint8_t *)&data);
-	//ti73_recv_ACK_h(calc_handle, NULL);
-	//ti73_recv_XDP_h(calc_handle, &length, buffer);
-	//ti73_send_ACK_h(calc_handle);
-	//if (fwrite(buffer, length, 1, f) < 1)
-	  //goto exit;
+        //printf("Receiving page %X out of %X\n", data[0], rampage+1);
+        //printf(" PC->TI: MEM REQ\n");
+        //dbus_send(calc_handle, PC_TI83p, 0x6F, 8, (uint8_t *)&data);
+        //ti73_recv_ACK_h(calc_handle, NULL);
+        //ti73_recv_XDP_h(calc_handle, &length, buffer);
+        //ti73_send_ACK_h(calc_handle);
+        //if (fwrite(buffer, length, 1, f) < 1)
+          //goto exit;
       //}
-exit:
-  fclose(f);
-  }
 
+exit:
+    fclose(f);
+  }
+exit2:
   tt_exit();
   return 0;
 }
